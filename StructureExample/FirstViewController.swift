7aac94eda7e3a120a33e6e7e01d110fcdc746125
Samuel Goodwin <samuel@roundwallsoftware.com>
2019-06-14 13:41:17 +0200

    Initial Project
    This is just a sample project template, this is where a regular app
    might start.

---
+++ StructureExample/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  StructureExample
+//
+//  Created by Samuel Goodwin on 6/14/19.
+//  Copyright © 2019 Samuel Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StructureExample/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StructureExample
+//
+//  Created by Samuel Goodwin on 6/14/19.
+//  Copyright © 2019 Samuel Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
